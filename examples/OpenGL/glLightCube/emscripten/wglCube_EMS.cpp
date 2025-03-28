4b6b39d5d6cbda290bbb7a36a31c8513edb9fe30
BrutPitt <brutpitt@gmail.com>
2025-03-28 05:38:10 +0100

    vGizmo3D v4.0 - examples (lighted Cube)

--- examples/OpenGL/glLightCube/emscripten/wglCube_EMS.cpp
+++ examples/OpenGL/glLightCube/emscripten/wglCube_EMS.cpp
@@ -273,7 +273,7 @@ void initVGizmo3D()     // Settings to control vGizmo3D
     // track.setGizmoFeeling(1.0);              // 1.0 default,  > 1.0 more sensible, < 1.0 less sensible
 
     // setIdleRotSpeed(1.0)                     // If used Idle() feature (continue rotation on Idle) it set that speed: more speed > 1.0 ,  less < 1.0
-        track.setWheelScale(20);
+    // track.setWheelScale(1.0);                // scale on wheel rotation:  less < 1.0 < more
 
     // other settings if you need it
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
